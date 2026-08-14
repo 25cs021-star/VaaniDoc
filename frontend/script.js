953c83e53fca0b630d60f6dee88e08499f4320a9
Dkhub23 <kakadiadhruvi34@gmail.com>
2026-08-14 10:40:18 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://vaanidoc-vwtv.onrender.com";
 
 const submitButton = document.getElementById("submitButton");
 const voiceButton = document.getElementById("voiceButton");
